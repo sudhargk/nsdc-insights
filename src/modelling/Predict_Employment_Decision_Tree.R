c6a5c44e9ec7f68b5ee57043562fdd3bded9f56a
vijayala8@gmail.com <vijayala8@gmail.com>
2019-05-02 06:44:14 +0000

    Predict_Employment_Decision_Tree.R updated

--- src/modelling/Predict_Employment_Decision_Tree.R
+++ src/modelling/Predict_Employment_Decision_Tree.R
@@ -7,89 +7,36 @@ library(caret)
 library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
-###nsdc <- read.csv("D:/INDESXD1 dat11A/Praxair/PG/Capstone Project/NSDC/NSDC_UChicago_Group1(1).csv",stringsAsFactors = F,header=T)
-#nsdc <-
-#  read_xlsx(
-#    "D:/INDESXD1 datA/Praxair/PG/Capstone Project/NSDC/NSDC_UChicago_Group1(1).xlsx",
-#    sheet = "NSDC_UChicago_Group1(1)"
-#  )
-input_file <- "/Users/vambavar/Documents/Vijaya/technical/data-Science/Jigsaw-Academy/capstone-project/NSDC_UChicago_Group1(1).csv"
-dat1 <- read.csv(input_file, stringsAsFactor=F, header=T)
-colnames(dat1)[colnames(dat1)=="X...NewID"] <- "NewID"
+library("readxl")
 
-#View(dat1)
-dat1$PlacementStatus[is.na(dat1$PlacementStatus)] = 0
-dat1$PlacementStatus<-ifelse(dat1$PlacementStatus==2,1,0)
-summary(dat1)
+input_file <- "/Users/vambavar/Documents/Vijaya/technical/data-Science/Jigsaw-Academy/capstone-project/NSDC_UChicago_Group1.xlsx"
+dat <- read_xlsx(input_file, sheet = "NSDC_UChicago_Group1(1)")
+summary(dat)
 
-dat1$Type.of.partner<-as.factor(dat1$Type.of.partner)
-#summary(dat1$PlacementStatus)
+#pre-processing
+dat$PlacementStatus[is.na(dat$PlacementStatus)] = 1
+dat$PlacementStatus<-as.factor(dat$PlacementStatus)
+dat$Gender<-as.factor(dat$Gender)
+dat$PreTrainingStatus[is.na(dat$PreTrainingStatus)] = 1
+dat$PreTrainingStatus<-as.factor(dat$PreTrainingStatus)
+dat$TechnicalEducation[is.na(dat$TechnicalEducation)] = 2
+dat$TechnicalEducation<-as.factor(dat$TechnicalEducation)
+dat$FundingPartner<-as.factor(dat$FundingPartner)
 
-# summary(dat1$VM1)
-# setting the missing value with most frequent value
-dat1$VM1[is.na(dat1$VM1)] = 1
-# summary(dat1$VM1)
-
-# summary(dat1$VM2)
-# setting the missing value with most frequent value
-dat1$VM2[is.na(dat1$VM2)] = 1
-# summary(dat1$VM2)
-
-# summary(dat1$Type.of.partner)
-dat1$Type.of.partner<-as.factor(dat1$Type.of.partner)
-# summary(dat1$Type.of.partner)
-
-# summary(dat1$Gender)
-dat1$Gender<-as.factor(dat1$Gender)
-# summary(dat1$Gender)
-
-# summary(dat1$PreTrainingStatus)
-attr1 <- dat1$PreTrainingStatus
-# setting the missing value with most frequent value
-dat1$PreTrainingStatus[is.na(dat1$PreTrainingStatus)] = 1
-dat1$PreTrainingStatus<-as.factor(dat1$PreTrainingStatus)
-# summary(dat1$PreTrainingStatus)
-
-# summary(dat1$TechnicalEducation)
-attr1 <- dat1$TechnicalEducation
-# setting the missing value with most frequent value
-dat1$TechnicalEducation[is.na(dat1$TechnicalEducation)] = 1
-dat1$TechnicalEducation<-as.factor(dat1$TechnicalEducation)
-# summary(dat1$TechnicalEducation)
-
-# summary(dat1$FundingPartner)
-dat1$FundingPartner<-as.factor(dat1$FundingPartner)
-# summary(dat1$FundingPartner)
-
-# summary(dat1$CentreType)
-dat1$CentreType<-as.factor(dat1$CentreType)
-# summary(dat1$CentreType)
-
-# summary(dat1$centre.Status)
-# setting the missing value with most frequent status
-dat1$centre.Status[is.na(dat1$centre.Status)] = 1
-dat1$centre.Status<-as.factor(dat1$centre.Status)
-# summary(dat1$centre.Status)
-
-# summary(dat1$CentreDistrict)
-# setting the missing value with most frequent value
-dat1$CentreDistrict[is.na(dat1$CentreDistrict)] = 1
-# summary(dat1$CentreDistrict)
-
-# drop unwanted predictors from input dat1a to modeling
-dat2 = subset(dat1, select=-c(NewID, Account.ID))
-
+# build the first model with many of the predictors since we don't know which has more co-efficient
+# drop unwanted predictors from input data
+dat1 = subset(dat, select=-c(NewID, Account.ID))
 # droppping batchID,  Course.Fee, Certified, Employment.Type, StateOFPlacementorWork, 
 # MonthlyEarningOrCTCbeforeTraining, MonthlyCurrentCTCOrearning since it has many NA's
-dat2 = subset(dat2, select=-c(batchID, Course.Fee, Certified, Employment.Type, StateOFPlacementorWork,
+dat1 = subset(dat1, select=-c(batchID, `Course Fee`, Certified, Employment.Type, StateOFPlacementorWork,
                               MonthlyEarningOrCTCbeforeTraining, MonthlyCurrentCTCOrearning))
-
 #dropping dates related predictors as of now
-dat2 = subset(dat2, select=-c(DateOfBirth, BatchStartDate, BatchEndDate))
-summary(dat2)
+dat1 = subset(dat1, select=-c(DateOfBirth, BatchStartDate, BatchEndDate))
 
-mod1<-rpart(PlacementStatus~.,data=dat2,
-           control=rpart.control(cp=0.01,maxdepth=10),method="class",parms=list(split="gini"))
+summary(dat1)
+# build the model with rpart()
+mod1<-rpart(PlacementStatus~.,data=dat1,
+            control=rpart.control(cp=0.01,maxdepth=10),method="class",parms=list(split="gini"))
 plot(mod1, margin=0.1, main="Classification Tree for NSDC Training and Employment Data")
 text(mod1, use.n=TRUE, all=TRUE, cex=.7)
 
@@ -98,41 +45,51 @@ fancyRpartPlot(mod1)
 printcp(mod1)
 plotcp(mod1, minline = TRUE)
 
-mod2<-prune(mod1,cp= 0.05)
-fancyRpartPlot(mod2)
-
-#Rules derivation
-mod2
-
 #Confusion Matrix
-actual<-dat2$PlacementStatus
-actual<-as.factor(actual)
-
-predicted<-predict(mod2,type = "class")
-head(predicted)
-head(as.numeric(predicted))
-predicted<-as.factor(predicted)
-predicted<-ifelse(predicted==2,1,0)
-
-summary(predicted)
-summary(actual)
-class(predicted)
-class(actual)
-str(predicted)
-str(actual)
-confusionMatrix(factor(predicted, levels = 0:1),
-                factor(predicted, levels = 0:1),
-                positive="1")
-
+actual1<-dat1$PlacementStatus
+predicted1<-predict(mod1,type = "class")
+confusionMatrix(predicted1,actual1,positive="1")
+#Accuracy : 0.7544
 #kappa metric
-kappa2(data.frame(actual,predicted))
+kappa2(data.frame(actual1,predicted1))
+#Kappa = 0.507
 
-#ROC curve analysis
+# data skewness is high. so, select some predictors only based on its mode, others are as is.
+dat%>%mutate(VM1_1=ifelse(VM1==1,1,0))->dat
+dat$VM1_1[is.na(dat$VM1_1)] = 0
+dat$VM1_1<-as.factor(dat$VM1_1)
+dat%>%mutate(VM2_8=ifelse(VM2==8,1,0))->dat
+dat$VM2_8[is.na(dat$VM2_8)] = 0
+dat$VM2_8<-as.factor(dat$VM2_8)
+dat%>%mutate(Grade_A=ifelse(Grade=='A',1,0))->dat
+dat$Grade_A[is.na(dat$Grade_A)] = 0
+dat$Grade_A<-as.factor(dat$Grade_A)
+dat%>%mutate(Skilling.Category_29=ifelse(Skilling.Category==29,1,0))->dat
+dat$Skilling.Category_29<-as.factor(dat$Skilling.Category_29)
+
+dat2 = subset(dat, select=c(PlacementStatus, Gender, VM1_1,VM2_8, PreTrainingStatus, TechnicalEducation,
+                            FundingPartner, Grade_A, Skilling.Category_29, CentreID,
+                            Course.MAster.ID, CandidateState))
+mod2<-rpart(PlacementStatus~.,data=dat2,
+           control=rpart.control(cp=0.01,maxdepth=10),method="class",parms=list(split="gini"))
+
+actual2<-dat2$PlacementStatus
+predicted2<-predict(mod2,type = "class")
+confusionMatrix(predicted2,actual2,positive="1")
+kappa2(data.frame(actual2,predicted2))
+#Accuracy : 0.7464
+#Kappa = 0.4895
+
+# since the first model has better accuracy, we'll consider it as final model.
 library(ROCR)
-#pred<-prediction(actual,predicted)
-#perf<-performance(pred,"tpr","fpr")
-#plot(perf,col="red")
-#abline(0,1, lty = 8, col = "grey")
-
-#auc<-performance(pred,"auc")
-#unlist(auc@y.values)
+pred1<-prediction(as.numeric(actual1),as.numeric(predicted1))
+perf1<-performance(pred1,"tpr","fpr")
+plot(perf1,col="red")
+abline(0,1, lty = 8, col = "grey")
+auc1<-performance(pred1,"auc")
+# auc is an s4 object, we are interested in "y.values" in this output
+auc1@y.values
+# y.values is of type list, so unlist it to get the value. y.values of value above 0.6 means it's a 
+# decent classification model.
+unlist(auc1@y.values)
+# 0.7558017
